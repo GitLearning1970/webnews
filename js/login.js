02d2b1a08f7d75dc117e1092eccebcb9223ba6c3
GitLearning <test.acc1970@outlook.com>
2025-10-02 22:04:27 -0300

    Login screen opens and closes with JavaScript

---
+++ js/login.js
@@ -0,0 +1,6 @@
+const loginScreen = document.getElementById('loginModal').style
+
+// open login screen
+loginBtn.addEventListener('click', () => loginScreen.display = 'block')
+// close login screen
+closeModal.addEventListener('click', () => loginScreen.display = 'none')
