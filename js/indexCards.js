febab88bc51b286b1d64582ff8d0f7cfa95f5eec
GitLearning <test.acc1970@outlook.com>
2025-10-03 11:36:25 -0300

    Generate news with JavaScript

--- js/indexCards.js
+++ js/indexCards.js
@@ -5,21 +5,42 @@ const divNews = document.createElement('div')
 divNews.classList.add('news')
 const main = document.getElementById('mainNews')
 
+let cardNum = 0
 const generateNews = function() {
     const row = []
     const card = []
+    const cardImg = []
+    const cardBody = []
+    const cardFooter = []
     // amount of rows
-    const rowsAmount = 6
+    const rowsAmount = 10
     for (let i = 0; i < rowsAmount; i++) {
         // generate news
         row[i] = document.createElement('div')
         row[i].classList.add('row')
         // amount of cards
         const cardsAmount = rand(3, 5)
-        // generate cards
         for (let j = 0; j < cardsAmount; j++) {
+            // create cards
             card[j] = document.createElement('div')
             card[j].classList.add('card')
+
+            cardNum++
+            // create images
+            cardImg[j] = document.createElement('img')
+            cardImg[j].src = `https://picsum.photos/200?random=${cardNum}`
+            cardImg[j].classList.add('card-img')
+            // create card bodies
+            cardBody[j] = document.createElement('div')
+            cardBody[j].classList.add('card-body')
+            cardBody[j].innerHTML = `<h4 class="card-title">Title ${cardNum}</h4>`
+            // create card footers
+            cardFooter[j] = document.createElement('div')
+            cardFooter[j].classList.add('card-footer')
+            cardFooter[j].innerHTML = '<p class="card-footer-category">category</p>'
+            cardFooter[j].innerHTML += '<p class="card-footer-date">DD / MM / AAAA</p>'
+            // put image, body and footer in the card
+            card[j].append(cardImg[j], cardBody[j], cardFooter[j])
             // put the cards in each row
             row[i].appendChild(card[j])
         }
