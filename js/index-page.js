52605f3ed55950b378c85b7c6193581d98c75d70
GitLearning <test.acc1970@outlook.com>
2026-02-18 10:35:06 -0300

    Renamed files

--- js/index-page.js
+++ js/index-page.js
@@ -53,3 +53,17 @@ function generateNews() {
 }
 
 generateNews()
+
+// // ======== DROPDOWN BUTTON ========
+const btn = document.querySelector('.dropdown-btn')
+const dropCont = document.querySelector('.dropdown-cont').style
+
+// open and close dropdown content
+btn.addEventListener('click', () => dropCont.display === 'none' ? dropCont.display = 'block' : dropCont.display = 'none')
+
+// close dropdown content by clicking outside
+addEventListener('click', (ev) => {
+    if (!ev.target.matches('.dropdown-btn') && dropCont.display === 'block') {
+        dropCont.display = 'none'
+    }
+})
